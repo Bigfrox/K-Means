d592d0ecf1b68a01eacafda036963c0bdc2a053a
Bigfrox <bigfroxgx@gmail.com>
2021-10-10 20:55:20 +0900

    it works

--- as2.py
+++ as2.py
@@ -2,11 +2,8 @@
 Data Mining Assignment 2, K-Means
 2016253072
 명수환(Myeong Suhwan)
-
 '''
 
-import re
-import random
 import time
 import math
 
@@ -28,8 +25,6 @@ def getDataFromFile(filename):
 
 def randClustering(gene_id):
     cluster = list() # k개의 cluster 생성, k : 10
-    
-    k = 10
     init_mem_num_in_cluster = int(len(gene_id) / k)
     line_num = 0
 
@@ -45,147 +40,158 @@ def randClustering(gene_id):
 def getKMeans(cluster,cluster_num,gene_id):
     kmeans = [0,0,0,0,0,0,
                 0,0,0,0,0,0,] #* 12-dimension
-    #print(gene_id[cluster[0][0]][1])
-    #print("cluster",cluster_num," => ",cluster[cluster_num])
-    if len(cluster[cluster_num]) == 1:
-        return gene_id[cluster[cluster_num][0]]
+    object_num = len(cluster[cluster_num])
 
     for i in range(DIMENSION):
-        #print(float(gene_id[cluster[0][0]][i]) - float(gene_id[cluster[0][1]][i]))
-        tmp = float(gene_id[cluster[cluster_num][0]][i]) + float(gene_id[cluster[cluster_num][1]][i])
-        tmp /= 2
-        kmeans[i] = tmp
-    #print("sum : ",kmeans)
-    
+        
+        tmp = 0
+        for num in range(object_num):
+            
+            tmp += (float(gene_id[cluster[cluster_num][num]][i]))
+            
+        tmp /= float(object_num)
+        
+        kmeans[i] = float("{:.3f}".format(tmp))
+        
+    #*print("k-means : ", kmeans)
     return kmeans
 
 def getDistance(kmeans, object):
     dist = 0
-    #print("kmeans : ", kmeans)
-    #print("data object : ", object)
+    
     for dimension in range(DIMENSION):
         dist_dimesion = float(kmeans[dimension]) - float(object[dimension])
-        tmp = pow(dist_dimesion,2)
+        tmp = math.pow(dist_dimesion,2)
         dist += tmp
     dist = math.sqrt(dist)
-    
-    return dist
+    #print(dist)
+    dist = "{:.3f}".format(dist)
+    #print(type(dist))
+    #print(float(dist))
+    return float(dist)
 
 def Reassign(obj_num, to,from_cluster_num,cluster):
-    print(obj_num,"이", "cluster",to,"에 추가되었습니다.")
+    # * add to new cluster
+    cluster[to].append(obj_num) # * number of object
 
-    #* add to new cluster
-    cluster[to].append(obj_num)#obj말고 obj의 라인 넘버를 줘야함
-    #print(cluster[to])
-    #* remove from old cluster
-    #print(cluster[from_cluster_num])
     
+    # * remove from old cluster
     cluster[from_cluster_num].remove(obj_num)
-    #print(cluster[from_cluster_num])
+    #print(obj_num,"이", "cluster",to,"에 추가되었습니다.")
 
+def output_to_file(filename,cluster):
+    file = open(filename, 'w')
+    
+    for i in range(k):
+        file.write('{0}: '.format(len(cluster[i])))
+        for v in cluster[i]:
+            file.write(str(v)+" ")
+        file.write("\n")
+        
 
+    file.close()
+    print("Finished to print to output file : ", filename)
 
 def main():
     start_time = time.time()
     global DIMENSION,k
     DIMENSION = 12
     k = 10
-    filename = 'assignment2_input.txt' #500
-    #filename = 'test1.txt'
+    input_filename = 'assignment2_input.txt' #500
+    #input_filename = 'test1.txt'
 
-    gene_id = getDataFromFile(filename)
-    #print(len(gene_id))
+    output_filename = 'assignment2_output.txt'
+
+    gene_id = getDataFromFile(input_filename)
+    print(gene_id)
+    ##*print(len(gene_id))
     cluster = randClustering(gene_id)
     print("Start . . . ")
     print(cluster)
+
     for num in range(k):
         print("cluster",num,cluster[num])
         for i in range(len(cluster[num])):
             print(gene_id[cluster[num][i]])
         print("\n")
-    #print(cluster)
-    #print("cluster",0,":",cluster[0][0], cluster[0][1])
+    ##*print(cluster)
 
     # * get K-means value at all clusters
     
-    change = True
+    isChanged = True # * default value is True
     count_for_debug = 0
-    while change:
-        print("=============================================\n\n\n")
-        print(count_for_debug+1,"번 반복하였습니다.")
-        count_for_debug += 1
-        change = False
-        for cluster_num in range(k):
-                
+    while isChanged:  
             
-            print("Cluster : ", cluster_num)
-            print("클러스터 내 오브젝트의 개수",len(cluster[cluster_num]))
+        print("=============================================")
+        
+        count_for_debug += 1
+        print(count_for_debug,"번 반복하였습니다.")
+        isChanged = False
+        for cluster_num in range(k): # * A cluster 
+                
+            if not cluster[cluster_num]:
+                ##*print("Cluster",cluster_num,"is empty cluster.")
+                continue
+            #*#*print("Cluster", cluster_num)
+            #*#*print("The number of objects in Cluster : ",len(cluster[cluster_num]))
+            kmeans1 = getKMeans(cluster,cluster_num,gene_id) # [0,1]
+            #print(cluster[cluster_num])
             iter_index = 0
-            while iter_index < len(cluster[cluster_num]):
-
-                for i in range(k):
-                    if cluster_num == i:
+            while iter_index < len(cluster[cluster_num]): # * iter_index is index of an element of a cluster
+                
+                distance = getDistance(kmeans1,gene_id[cluster[cluster_num][iter_index]]) #! 
+                #print("클러스터 ",cluster_num,"의 오브젝트 : ", cluster[cluster_num][iter_index])
+                #print("거리: ",distance)
+                for i in range(k): # * other cluster to compare
+                    # * Find the shorter distance with other K-Means of other clusters with loop
+                    if cluster_num == i: # * Don't need to compare with myself 
                         continue
-                    if not (cluster[cluster_num] and cluster[i]):
+                    if not cluster[i]: # * pass the empty cluster
+                        #*print("Cluster",i,"is empty cluster.")
                         continue
                     
-                    kmeans = getKMeans(cluster,cluster_num,gene_id) # [0,1]
-                    #print(cluster[cluster_num])
-                    #print(iter_index)
-                    distance = getDistance(kmeans,gene_id[cluster[cluster_num][iter_index]]) #!
-                    #print("distance : ",distance) # * distance between k-means and an object in a cluster
-                    #print("kmeans :",kmeans)
-                    
-                    #print("cluster num: ",cluster_num,cluster[cluster_num],"with cluster ", i)
                     if cluster[cluster_num] and cluster[i]:
                         
-                        kmeans = getKMeans(cluster,i,gene_id)#* i cluster의 K-Means
+                        kmeans2 = getKMeans(cluster,i,gene_id)#* K-Means of i-cluster
+                        dist = getDistance(kmeans2,gene_id[cluster[cluster_num][iter_index]]) # * Distance to K-Means of i-cluster
                         
-                    
                         
-                        dist = getDistance(kmeans,gene_id[cluster[cluster_num][iter_index]]) # * i cluster의 K-Means까지의 Distance
-                        
-                        #print("dist : ",dist)
-                        #print(cluster[cluster_num][iter_index],"오브젝트에 대하여 ", end=" ")
-                        #print("cluster",cluster_num,"의 K-Means와","cluster",i,"의 K-Means중 가까운 것을 찾는 중입니다.")
-                        if dist < distance:
-                            print(cluster[cluster_num][iter_index],"오브젝트에 대하여 ", end=" ")
-                            print("cluster",cluster_num,"의 K-Means와","cluster",i,"의 K-Means중 가까운 것을 찾는 중입니다.")
-                            print("거리가 더 가까운 K-Means를 찾았습니다.")
-                            #print("shorter distance : ", dist)
-                            #print(">cluster : ", i)
+                        if dist < distance: # * after finding, Re-assign objects to clusters based on the distance that I've found.
+                            #print(cluster[cluster_num][iter_index],"오브젝트에 대하여 ", end=" ")
+                            #print("cluster",cluster_num,"의 K-Means와","cluster",i,"의 K-Means중 가까운 것을 찾는 중입니다.")
+                            #print("거리가 더 가까운 K-Means를 찾았습니다.")
+                            # print("\n오브젝트 ",cluster[cluster_num][iter_index],"에 대하여 ")
+                            # print("원래 Cluster",cluster_num,"에서 K-Means까지의","Distance : ", distance)
+                            # print("비교할 Cluster",i,"에서 K-Means까지의","Distance : ", dist)
+                            # print(dist," < ", distance,"이므로 이동합니다.")
+                            
+                            
                             obj_num = cluster[cluster_num][iter_index]
-                            #print("obj number>>",obj_num)
+                            
                             to = i
                             Reassign(obj_num,to,cluster_num,cluster)
-                            print("\n")
-                            change = True
+                            
+                            isChanged = True
+                            
                             iter_index -= 1
                             if iter_index < 0:
                                 iter_index = 0
-                            #! in here, Have to change to shortest cluster
+                            
                     
                     
                     else:
                         continue
 
                 iter_index += 1
-                
-        # cluster_num = 1
-        # kmeans = getKMeans(cluster,cluster_num,gene_id)
-        # distance = getDistance(kmeans,gene_id[cluster[cluster_num][0]])
-        # print(distance)
-        #? and then, find the shortest distance with other K-Means of other clusters.
-        #? after finding, Re-assign objects to clusters based on the distance that I've found.
-        
-        
 
-    #print(cluster)
     print("\n\n")
     for i in range(k):
         cluster[i].sort()
         print(i,"번 클러스터 :","SIZE: ",len(cluster[i]), cluster[i])
         print("\n")
+
+    output_to_file(output_filename,cluster)
+    
     print("Time Elapsed : ", time.time() - start_time)
 
 if __name__ == '__main__':
